2442daa042cfa27ec66ef7224f6a18c83b3d16de
asrevelo23 <32751263+asrevelo23@users.noreply.github.com>
2019-03-13 11:43:46 -0500

    Update loop.js

--- skills/loop.js
+++ skills/loop.js
@@ -1,34 +1,44 @@
 //
 // Example of a conversation with a menu that loops until explicitly stopped
 //
+
+
+
 module.exports = function (controller) {
 
-    controller.hears([/^loop$/], 'direct_message,direct_mention', function (bot, message) {
+    controller.hears([/^weather/], 'direct_message,direct_mention', function (bot, message) {
 
         bot.startConversation(message, function (err, convo) {
 
-            var question = "Here are a few proposed DevNet activities:";
-            question += "<br/> `1)` join a Community Of Interest (**communities**)";
-            question += "<br/> `2)` take a Learning Lab (**labs**)";
-            question += "<br/> `3)` check Upcoming Events (**events**)";
-            question += "\n\nWhat do you want to check?<br/>_(type a number, a **bold keyword** or `stop`)_";
+            var question = "Here are your most important cities:";
+            question += "<br/> `1)` Austin, Tx (**Austin**)";
+            question += "<br/> `2)` Houston, Tx (**Houston**)";
+            question += "<br/> `3)` San Salvador (**Salvador**)";
+            question += "<br/> `4)` La Libertad (**Libertad**)";
+            question += "\n\nWhat wearher do you want to check?<br/>_(type a number, a **bold keyword** or `stop`)_";
             convo.ask(question, [
                 {
-                    pattern: "1|community|communities",
+                    pattern: "1|Austin|austin",
                     callback: function (response, convo) {
                         convo.gotoThread('menu_1');
                     },
                 }
                 , {
-                    pattern: "2|lab|track|learn",
+                    pattern: "2|houston|Houston",
                     callback: function (response, convo) {
                         convo.gotoThread('menu_2');
                     },
                 }
                 , {
-                    pattern: "3|event|express",
+                    pattern: "3|San Salvador|Salvador|salvador",
                     callback: function (response, convo) {
                         convo.gotoThread('menu_3');
+                    },
+                }
+                , {
+                    pattern: "4|La Libertad|Libertad|libertad",
+                    callback: function (response, convo) {
+                        convo.gotoThread('menu_4');
                     },
                 }
                 , {
@@ -47,21 +57,27 @@ module.exports = function (controller) {
 
             // Menu option 1)
             convo.addMessage({
-                text: "Excellent choice: now [discover the DevNet communities](https://developer.cisco.com/site/coi/) online, and pick your favorite...",
+                text: "Lets see how Austin is doing today: now [discover Austin's weather](https://weather.com/weather/today/l/USTX0057:1:US). Be safe!",
                 action: 'default'
             }, 'menu_1');
 
             // Menu option 2)
             convo.addMessage({
-                text: "Learnings **labs** are step-by-step tutorials. They are grouped into **tracks** to help you on your rampup journey. Just browse through [the learnings tracks](https://learninglabs.cisco.com/login) and pick the labs that suits your learning appetite!",
+                text: "Lets see how HTown is doing today: now [discover Houston's weather](https://weather.com/weather/today/l/USTX0617:1:US). Be safe!",
                 action: 'default'
             }, 'menu_2');
 
             // Menu option 3)
             convo.addMessage({
-                text: "Nothing's like meeting in person at a conference, training or a hackathon. Check the list of [DevNet events](https://developer.cisco.com/site/devnet/events-contests/events/) or ask the bot: invite `CiscoDevNet@sparkbot.io` to chat in a Webex Teams space.",
+                text: "Lets see how San Salvador is doing today: now [discover San Salvador's weather](https://weather.com/weather/today/l/ESXX0003:1:ES). Be safe!",
                 action: 'default'
             }, 'menu_3');
+            
+            // Menu option 4)
+            convo.addMessage({
+                text: "Lets see how San Miguel is doing today: now [discover San Miguel's weather](https://weather.com/weather/today/l/ESXX0007:1:ES). Be safe!",
+                action: 'default'
+            }, 'menu_4');
 
             // Cancel
             convo.addMessage({
